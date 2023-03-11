bd978eaf4520354974439e86039a37201a1e2688
HusseinElbhrawy <hussein.elbhrawy74@gmail.com>
2023-03-11 18:12:33 +0200

    home ui

--- lib/modules/home/presentation/screens/home_screen.dart
+++ lib/modules/home/presentation/screens/home_screen.dart
@@ -19,30 +19,105 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: SvgPicture.asset(
-          AppSvgAssets.appLogo,
-          height: AppDimensions.height45(context),
+    return Padding(
+      padding: EdgeInsets.all(AppConstant.defaultPadding(context)),
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: SvgPicture.asset(
+              AppSvgAssets.appLogo,
+              height: AppDimensions.height45(context),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                const HomeSearchWidget(),
+                const HomeBannerWidget(),
+                AppConstant.horizontalDivider(),
+                const BigTextWidget(
+                  'Categories',
+                  fontWeight: AppFontsWeight.bold,
+                ),
+                const HomeCategoriesWidget(),
+                const BigTextWidget(
+                  'Products',
+                  fontWeight: AppFontsWeight.bold,
+                ),
+                AppConstant.horizontalDivider(),
+              ],
+            ),
+          ),
+          SliverGrid.builder(
+            itemCount: 15,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: AppDimensions.height20(context),
+              crossAxisSpacing: AppDimensions.width20(context),
+              childAspectRatio: .8,
+            ),
+            itemBuilder: (context, index) {
+              return const SingleProductItemWidget();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SingleProductItemWidget extends StatelessWidget {
+  const SingleProductItemWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey.shade300,
+        borderRadius: BorderRadius.circular(
+          AppConstant.defaultPadding(context) / 2,
         ),
       ),
-      body: ListView(
-        padding: EdgeInsets.all(
-          AppConstant.defaultPadding(context),
-        ),
+      child: Column(
         children: [
-          const HomeSearchWidget(),
-          const HomeBannerWidget(),
-          AppConstant.horizontalDivider(),
-          const BigTextWidget(
-            'Categories',
-            fontWeight: AppFontsWeight.bold,
+          Align(
+            alignment: AlignmentDirectional.topEnd,
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.favorite_border,
+                color: Colors.black,
+              ),
+            ),
           ),
-          const HomeCategoriesWidget(),
-          const BigTextWidget(
-            'Products',
-            fontWeight: AppFontsWeight.bold,
-          )
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Material(
+                clipBehavior: Clip.antiAlias,
+                borderRadius: BorderRadius.circular(
+                  AppConstant.defaultPadding(context) / 2,
+                ),
+                child: Image.asset(
+                  'assets/images/on_boarding_1.png',
+                  fit: BoxFit.fill,
+                  width: double.infinity,
+                ),
+              ),
+            ),
+          ),
+          Column(
+            children: const [
+              SmallTextWidget(
+                'Product Name',
+                fontWeight: AppFontsWeight.bold,
+              ),
+              BigTextWidget(
+                'Rs. 1000',
+                fontWeight: AppFontsWeight.bold,
+              ),
+            ],
+          ),
         ],
       ),
     );
@@ -139,56 +214,64 @@ class HomeSearchWidget extends StatelessWidget {
     return Row(
       children: [
         Expanded(
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.grey.shade300,
-              borderRadius: BorderRadius.circular(
-                AppConstant.defaultPadding(context) / 2,
-              ),
-            ),
-            child: TextFormField(
-              scrollPadding: EdgeInsets.zero,
-              textInputAction: TextInputAction.search,
-              keyboardType: TextInputType.text,
-              onFieldSubmitted: (value) {},
-              decoration: const InputDecoration(
-                hintText: 'Search',
-                prefixIcon: Icon(
-                  Icons.search,
-                  color: Colors.black,
+          child: Material(
+            elevation: 20,
+            color: Colors.transparent,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey.shade300,
+                borderRadius: BorderRadius.circular(
+                  AppConstant.defaultPadding(context) / 2,
                 ),
-                errorBorder: InputBorder.none,
-                enabledBorder: InputBorder.none,
-                focusedBorder: InputBorder.none,
-                disabledBorder: InputBorder.none,
-                focusedErrorBorder: InputBorder.none,
-                border: InputBorder.none,
+              ),
+              child: TextFormField(
+                scrollPadding: EdgeInsets.zero,
+                textInputAction: TextInputAction.search,
+                keyboardType: TextInputType.text,
+                onFieldSubmitted: (value) {},
+                decoration: const InputDecoration(
+                  hintText: 'Search',
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  ),
+                  errorBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  disabledBorder: InputBorder.none,
+                  focusedErrorBorder: InputBorder.none,
+                  border: InputBorder.none,
+                ),
               ),
             ),
           ),
         ),
         AppConstant.verticalDivider(),
-        IconButton(
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(
-              Colors.grey.shade300,
-            ),
-            shape: MaterialStatePropertyAll(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(
-                  AppConstant.defaultPadding(context) / 2,
+        Material(
+          elevation: 20,
+          color: Colors.transparent,
+          child: IconButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(
+                Colors.grey.shade300,
+              ),
+              shape: MaterialStatePropertyAll(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(
+                    AppConstant.defaultPadding(context) / 2,
+                  ),
+                ),
+              ),
+              padding: MaterialStateProperty.all(
+                const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 16,
                 ),
               ),
             ),
-            padding: MaterialStateProperty.all(
-              const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 16,
-              ),
-            ),
+            onPressed: () {},
+            icon: const Icon(Icons.filter_alt),
           ),
-          onPressed: () {},
-          icon: const Icon(Icons.filter_alt),
         ),
       ],
     );
